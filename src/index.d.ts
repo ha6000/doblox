ec4cad212d7c788971114d8b7fee3f1987a097b5
Harry <krugerh27@gmail.com>
2020-06-11 22:44:13 +0200

    Updating declarations

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,25 +1,39 @@
-import discord from 'discord.js';
-import noblox from 'noblox.js';
-
-type noblox = object | any;
-
-type UserResolvable = RobloxUser | discord.UserResolvable;
-
+import * as discord from 'discord.js';
+declare type noblox = object | any;
+declare type UserResolvable = RobloxUser | discord.UserResolvable;
 declare class RobloxUser {
-	username: string;
-	id: string
-	constructor(username: string, id: string) {
-		this.username = username;
-		this.id = id;
-	}
+    username: string;
+    id: string;
+    /**
+     * @param {string} username roblox username
+     * @param {string} id
+     */
+    constructor(username: string, id: string);
 }
-
-declare class Client {
-	noblox: noblox;
-	client: discord.Client;
-	_RateLimit: Promise<any>;
-	constructor(noblox: noblox, client: discord.Client);
-	RateLimit(limit?: number): Promise<any>;
-	getRobloxUser(user: discord.UserResolvable): Promise<RobloxUser | undefined>;
-	getRoleInGroup(player: UserResolvable, group: number): Promise<string>;
+/**
+ * A value that resolves to a discord user or roblox user
+ * @typedef {RobloxUser | discord.UserResolvable} UserResolvable
+ */
+export declare class Client {
+    noblox: noblox;
+    client: discord.Client;
+    _RateLimit: Promise<any>;
+    /**
+     * @param {noblox}         noblox Your noblox module object
+     * @param {discord.Client} client A discord client
+     */
+    constructor(noblox: noblox, client: discord.Client);
+    RateLimit(limit?: number): Promise<any>;
+    /**
+     * @param {discord.UserResolvable} user The discord user to get robloxUser of
+     */
+    getRobloxUser(user: discord.UserResolvable): Promise<RobloxUser | undefined>;
+    /**
+     * get a role of a user in a group
+     * @param  {UserResolvable}  player A roblox user or discord user
+     * @param  {string}          group  The group to check in
+     * @return {Promise<string>}        Promise resolving to role name
+     */
+    getRoleInGroup(player: UserResolvable, group: number): Promise<string>;
 }
+export {};
