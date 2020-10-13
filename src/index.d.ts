c5465b8884bd4e806145c7837ca9cd980ca6193f
Harry <krugerh27@gmail.com>
2020-10-13 10:48:22 +0200

    Updating declarations

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,39 +1,34 @@
+import * as axios from "axios";
 import * as discord from 'discord.js';
 declare type noblox = object | any;
-declare type UserResolvable = RobloxUser | discord.UserResolvable;
+declare type ClientOptions = {
+    provider: string;
+};
+declare const Limiter: any;
 declare class RobloxUser {
     username: string;
     id: string;
-    /**
-     * @param {string} username roblox username
-     * @param {string} id
-     */
-    constructor(username: string, id: string);
+    description: string;
+    createdAt: Date;
+    partial: Boolean;
+    constructor(data: any, partial: Boolean);
 }
-/**
- * A value that resolves to a discord user or roblox user
- * @typedef {RobloxUser | discord.UserResolvable} UserResolvable
- */
 export declare class Client {
     noblox: noblox;
     client: discord.Client;
-    _RateLimit: Promise<any>;
+    options: ClientOptions;
+    provider: any;
+    _RateLimit: typeof Limiter;
+    providerAxios: axios.AxiosInstance;
+    axios: axios.AxiosInstance;
     /**
-     * @param {noblox}         noblox Your noblox module object
+     * @param {noblox}         noblox Your noblox module object, not required but kept for backwards compatibility.
      * @param {discord.Client} client A discord client
      */
-    constructor(noblox: noblox, client: discord.Client);
-    RateLimit(limit?: number): Promise<any>;
+    constructor(noblox: noblox | undefined, client: discord.Client, options: ClientOptions);
     /**
      * @param {discord.UserResolvable} user The discord user to get robloxUser of
      */
-    getRobloxUser(user: discord.UserResolvable): Promise<RobloxUser | undefined>;
-    /**
-     * get a role of a user in a group
-     * @param  {UserResolvable}  player A roblox user or discord user
-     * @param  {string}          group  The group to check in
-     * @return {Promise<string>}        Promise resolving to role name
-     */
-    getRoleInGroup(player: UserResolvable, group: number): Promise<string>;
+    getRobloxUser(user: discord.UserResolvable, partial?: Boolean): Promise<RobloxUser | undefined>;
 }
 export {};
